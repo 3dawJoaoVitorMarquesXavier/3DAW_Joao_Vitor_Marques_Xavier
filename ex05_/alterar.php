4c5b54df9d8ed3657ba9f414c970d686ba9f62d7
João Vitor Marques Xavier <alun23104708360056@h1-02.faculdade.edu>
2024-09-10 11:03:39 -0300

    update

---
+++ ex05_/alterar.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Alterar</h1>
+
+    <form action="alterar2.php" method="GET">
+    <label>Sigla: </label>
+    <input type="text" name="sigla">
+    <input type="submit" value="enviar">
+    </form>
+    
+</body>
+</html>
